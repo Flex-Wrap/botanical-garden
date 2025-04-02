e37fe97be0f642ac69defd2714f17b4a2894e1c8
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2025-04-02 21:24:47 +0200

    test

--- src/pages/StorySelection.tsx
+++ src/pages/StorySelection.tsx
@@ -1,8 +1,6 @@
 import StoryCard from "../components/StoryCard";
-import { useNavigate } from "react-router-dom";
 
 function StorySelection() {
-    const navigate = useNavigate();
 
     return (
       <div className="h-full w-full flex flex-col items-center justify-center bg-red-900 text-white"
@@ -29,13 +27,6 @@ function StorySelection() {
             description="Do you want to hear interesting facts from our habitants?"
           />
         </div>
-        {/* Navigate Button */}
-        <button 
-          className="absolute bottom-4 bg-white text-red-900 px-6 py-2 rounded font-bold"
-          onClick={() => navigate("/map", { state: { showPopup: true } })}
-        >
-          Go to Map
-        </button>
       </div>
     );
 }
