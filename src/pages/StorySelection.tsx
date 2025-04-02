1478e5d5ab055a6614b1087f56ffd576066ccaa7
dia-avv <eaa24avv@students.eaaa.dk>
2025-04-02 19:44:26 +0200

    story selection done i think

--- src/pages/StorySelection.tsx
+++ src/pages/StorySelection.tsx
@@ -2,9 +2,30 @@ import StoryCard from "../components/StoryCard";
 
 function StorySelection() {
     return (
-      <div className="h-full w-full flex flex-col items-center justify-center bg-red-900 text-white">
-        <h1 className="text-2xl font-bold">This is the story selection page</h1>
-        <StoryCard icon="assets/romantic.svg" name="Romantic" description="Are you ready to fall in love with Botanical Garden?"/>
+      <div className="h-full w-full flex flex-col items-center justify-center bg-red-900 text-white"
+      style={{
+        backgroundImage: "url(assets/storyBG.png)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      >
+        <div className="flex flex-col gap-6">
+          <StoryCard 
+            icon="assets/romantic.svg" 
+            name="Romantic" 
+            description="Are you ready to fall in love with Botanical Garden?"
+          />
+          <StoryCard 
+            icon="assets/family.svg" 
+            name="Family" 
+            description="Let’s explore the Garden secret family."
+          />
+          <StoryCard 
+            icon="assets/academic.svg" 
+            name="Academic" 
+            description="Do you want to hear interesting facts from our habitants?"
+          />
+        </div>
       </div>
     );
   }
