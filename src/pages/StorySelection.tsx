7092233043e6ece99c770e4791154cfaf2027e06
dia-avv <eaa24avv@students.eaaa.dk>
2025-04-02 19:48:45 +0200

    story selection i think done

--- src/pages/StorySelection.tsx
+++ src/pages/StorySelection.tsx
@@ -29,10 +29,6 @@ function StorySelection() {
             description="Do you want to hear interesting facts from our habitants?"
           />
         </div>
-      <div className="h-full w-full flex flex-col items-center justify-center bg-red-900 text-white">
-        <h1 className="text-2xl font-bold">This is the story selection page</h1>
-        <StoryCard icon="assets/romantic.svg" name="Romantic" description="Are you ready to fall in love with Botanical Garden?"/>
-        
         {/* Navigate Button */}
         <button 
           className="absolute bottom-4 bg-white text-red-900 px-6 py-2 rounded font-bold"
